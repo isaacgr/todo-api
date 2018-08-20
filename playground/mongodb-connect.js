a3a1c325500a272633e65b362157ed5a8221339c
isaacgr <isaacgrowell@gmail.com>
2018-08-20 07:35:55 -0400

    Initialize repo

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,41 @@
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to server");
+    }
+    console.log("Connected to server");
+    const db = client.db("TodoApp");
+
+    // db.collection("Todos").insertOne(
+    //   {
+    //     text: "Something to do",
+    //     completed: false
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Could not insert data", error);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //   }
+    // );
+
+    db.collection("Users").insertOne(
+      {
+        name: "Isaac",
+        age: 26,
+        location: "Hamilton"
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Cound not insert data");
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+      }
+    );
+
+    client.close();
+  }
+);
