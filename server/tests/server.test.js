5ad13ef74e88586678d41477bbbc4722e83574b4
isaacgr <isaacgrowell@gmail.com>
2018-09-05 07:34:31 -0400

    Add test cases for variable todo id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,10 +1,14 @@
 const expect = require("expect");
 const request = require("supertest");
+const { ObjectID } = require("mongodb");
 
 const { app } = require("../server");
 const { Todo } = require("../models/todo");
 
-const todos = [{ text: "First todo" }, { text: "Second todo" }];
+const todos = [
+  { _id: new ObjectID(), text: "First todo" },
+  { _id: new ObjectID(), text: "Second todo" }
+];
 
 beforeEach(done => {
   Todo.remove({})
@@ -69,6 +73,33 @@ describe("GET /todos", () => {
       .expect(response => {
         expect(response.body.todos.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe("GET /todos/:id", () => {
+  it("should return todo doc", done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(response => {
+        expect(response.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it("should return 404 if todo not found", done => {
+    const id = new ObjectID();
+    request(app)
+      .get(`/todos/${id}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return 404 for invalid id", done => {
+    request(app)
+      .get("/todos/123")
+      .expect(404)
       .end(done);
   });
 });
